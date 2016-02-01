84707f9f2898e9a3285fe900c0bdd4f664fc1981
Harry Oliver <someonecancode@yahoo.co.uk>
2016-02-01 20:22:13 +0000

    Add before action to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
+require 'mailchimp'
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_action :setup_mcapi
+  
+  def setup_mcapi
+    @mc = Mailchimp::API.new('MAILCHIMP_API_KEY')
+    @list_id = "LIST ID"
+  end
+  
 end
